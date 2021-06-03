400c54687160bdff420c8073419e119f053c1394
Paul <git@paul.sx>
2021-06-03 16:31:37 -0400

    New and edit item links and pages

--- things/urls.py
+++ things/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('', views.LocationListView.as_view(), name='locations'),
     path('location/<int:pk>/', views.LocationDetailView.as_view(), name='location'),
     path('location/new/', views.LocationCreateView.as_view(), name='location_create'),
+    path('location/<int:pk>/edit/', views.LocationUpdateView.as_view(), name='location_edit'),
     path('location/<int:location_pk>/container/new/', views.ContainerCreateView.as_view(), name='container_create'),
     path('location/<int:location_pk>/container/<int:pk>/', views.ContainerDetailView.as_view(), name='container'),
     path('location/<int:location_pk>/container/<int:container_pk>/thing/<int:pk>/', views.ThingDetailView.as_view(), name='thing'),
+    path('location/<int:location_pk>/container/<int:pk>/edit/', views.ContainerUpdateView.as_view(), name='container_edit'),
+    path('location/<int:location_pk>/container/<int:container_pk>/thing/new/', views.ThingCreateView.as_view(), name='thing_create'),
+    path('location/<int:location_pk>/container/<int:container_pk>/thing/<int:pk>/edit/', views.ThingUpdateView.as_view(), name='thing_edit'),
 ]
--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from things.models import Location, Container, Thing
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 from django.views.generic.edit import CreateView
 from things.forms import LocationForm
 
@@ -22,6 +22,15 @@ class LocationDetailView(DetailView):
         context['containers'] = Container.objects.filter(location=self.get_object())
         return context
 
+class LocationCreateView(CreateView):
+    model = Location
+    fields = ['name',]
+    template_name = 'things/location_create.html'
+
+class LocationUpdateView(UpdateView):
+    model = Location
+    fields = ['name',]
+    template_name = 'things/location_edit.html'
 
 class ContainerDetailView(DetailView):
     model = Container
@@ -31,12 +40,20 @@ class ContainerDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['things'] = Thing.objects.filter(container=self.get_object())
+        context['location'] = get_object_or_404(Location, pk=self.kwargs.get('location_pk'))
         return context
+        
 
-class LocationCreateView(CreateView):
-    model = Location
-    fields = ['name',]
-    template_name = 'things/location_create.html'
+class ContainerUpdateView(UpdateView):
+    model = Container
+    fields = ['name', 'location']
+    context_object_name = 'container'
+    template_name = 'things/container_edit.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['location'] = get_object_or_404(Location, pk=self.kwargs.get('location_pk'))
+        return context
 
 
 class ContainerCreateView(CreateView):
@@ -56,6 +73,37 @@ class ThingDetailView(DetailView):
     template_name = 'things/thing_detail.html'
     context_object_name = 'thing'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['container'] = get_object_or_404(Container, pk=self.kwargs.get('container_pk'))
+        context['location'] = get_object_or_404(Location, pk=self.kwargs.get('location_pk'))
+        return context
+
+
+class ThingCreateView(CreateView):
+    model = Thing
+    fields = ['name', 'description', 'container']
+    template_name = 'things/thing_create.html'
+
+    def get_initial(self):
+        container = get_object_or_404(Container, pk=self.kwargs.get('container_pk'))
+        return {
+            'container': container
+        }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['container'] = get_object_or_404(Container, pk=self.kwargs.get('container_pk'))
+        context['location'] = get_object_or_404(Location, pk=self.kwargs.get('location_pk'))
+        return context
+
+
+class ThingUpdateView(UpdateView):
+    model = Thing
+    fields = ['name', 'description', 'container']
+    template_name = 'things/thing_edit.html'
+    context_object_name = 'thing'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['container'] = get_object_or_404(Container, pk=self.kwargs.get('container_pk'))
