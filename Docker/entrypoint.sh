f2171c5d013df40b3020bf7e61f4a1b7ba4eb299
Paul <git@paul.sx>
2021-06-06 21:57:16 -0400

    Fix entrypoint commands

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -5,11 +5,11 @@ cd project
 if [ ! -f manage.py ]; then
     git clone https://github.com/paul-sx/storage.git .
     python manage.py migrate
-    python manage.py makestatic
+    python manage.py collectstatic
 else
     git pull
     python manage.py migrate
-    python manage.py makestatic
+    python manage.py collectstatic
 fi
 
 exec "$@"
