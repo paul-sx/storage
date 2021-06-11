b338fd78f4a6bcc8e34bc6d21618526c1e555e3c
Paul <git@paul.sx>
2021-06-10 20:14:18 -0400

    Entrypoint edit for noinput

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,12 +4,12 @@ cd project
 
 if [ ! -f manage.py ]; then
     git clone https://github.com/paul-sx/storage.git .
-    python manage.py migrate
-    python manage.py collectstatic
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput
 else
     git pull
-    python manage.py migrate
-    python manage.py collectstatic
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput
 fi
 
 exec "$@"
